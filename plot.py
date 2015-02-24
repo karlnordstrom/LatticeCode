e223e62ff381bb4e2d58d9b04090bc524b2cde33
Karl Nordstrom <k.nordstrom.1@research.gla.ac.uk>
2015-02-24 19:12:47 +0000

    plotting.

--- plot.py
+++ plot.py
@@ -15,11 +15,12 @@ improved_val = improved[:,1]
 improved_err = improved[:,2]
 
 plt.plot((0.,3.), (2.,2.), 'black')
-plt.errorbar(normal_t/4-0.01, normal_val, yerr=normal_err, color='blue', linestyle='None', label="$1^{st} order")
-plt.errorbar(improved_t/4+0.01, improved_val, yerr=improved_err, color='red', linestyle='None', label="$2^{nd} order")
+plt.errorbar(normal_t/4-0.01, normal_val, yerr=normal_err, color='blue', linestyle='None', label="$1^{st}$ order")
+plt.errorbar(improved_t/4+0.01, improved_val, yerr=improved_err, color='red', linestyle='None', label="$2^{nd}$ order")
+plt.legend(loc=0, numpoints=1)
 plt.xlabel("$t$")
 plt.ylabel("$\Delta E(t)$")
 plt.title("Results using $1^{st}$ and $2^{nd}$ order finite difference")
 
 
-plt.savefig("comparison.png")
+plt.savefig("comparison.png", dpi=300)
